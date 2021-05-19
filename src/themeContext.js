4c0a4531ff292fd13d04af2e8e384733fbddc257
Adriana Ito <ito.dri@gmail.com>
2021-05-19 12:13:46 +0900

    change theme context to function component

--- src/themeContext.js
+++ src/themeContext.js
@@ -1,27 +1,22 @@
-import React, {Component} from 'react'
+import React, {useState} from 'react'
 
 const ThemeContext = React.createContext()
 
-class ThemeContextProvider extends Component {
-  state = {
-    theme: 'dark'
-  }
+function ThemeContextProvider(props) {
+  const [theme, setTheme] = useState("dark")
 
-  toggleTheme = () => {
-    this.setState(prevState => {
-        return {
-            theme: prevState.theme === "light" ? "dark" : "light"
-        }
+  function toggleTheme() {
+    setTheme(prevTheme => {
+        return prevTheme === "light" ? "dark" : "light"
     })
   }
 
-  render() {
-    return (
-      <ThemeContext.Provider value={{theme: this.state.theme, toggleTheme: this.toggleTheme}}>
-          {this.props.children}
-      </ThemeContext.Provider>
-    )
-  }
+  return (
+    <ThemeContext.Provider value={{theme: theme, toggleTheme: toggleTheme}}>
+        {props.children}
+    </ThemeContext.Provider>
+  )
+
 }
 
 export {ThemeContextProvider, ThemeContext}
