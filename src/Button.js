a40f6a5d12d7b1f4111ff29dfa25e9e5d93aa22f
Adriana Ito <ito.dri@gmail.com>
2021-05-19 11:43:53 +0900

    add button component

---
+++ src/Button.js
@@ -0,0 +1,17 @@
+import React, {useContext} from 'react'
+import { ThemeContext } from './themeContext'
+
+function Button() {
+  const {theme, toggleTheme} = useContext(ThemeContext)
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className={`${theme}-theme`}
+    >
+      Switch Theme
+    </button>
+  )
+}
+
+export default Button
